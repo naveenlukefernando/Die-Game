9a743a90c291686f37483ad6ffc7e2162f4c0ed4
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-12-05 14:42:30 +0530

    Done 2.42

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
 
-*/
-
-var scores,roundScore, activePlayer ;
+var scores,roundScore, activePlayer,gamePlaying ;
 
 gameInitit ();
 
@@ -31,7 +22,9 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click',function () {
 
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if (gamePlaying)
+    {
+        var dice = Math.floor(Math.random() * 6) + 1;
     console.log(dice);
 
     var diceDOM = document.querySelector('.dice');
@@ -48,13 +41,20 @@ document.querySelector('.btn-roll').addEventListener('click',function () {
     else {
 
                 nextPlayer();
-}
+        }
+
+    }
+
+    
+    
     
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function() {
 
-    scores [activePlayer] += roundScore ; 
+        if (gamePlaying) {
+
+            scores [activePlayer] += roundScore ; 
 
     document.getElementById('score-'+ activePlayer).textContent = scores[activePlayer];
 
@@ -65,14 +65,13 @@ document.querySelector('.btn-hold').addEventListener('click',function() {
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-'+ activePlayer + '-panel').classList.add("winner");
         document.querySelector('.player-'+ activePlayer + '-panel').classList.remove("active");
+        gamePlaying = false;
     }
     else{
         nextPlayer();
     }
 
-
-
-   
+}
 
 });
 
@@ -104,6 +103,7 @@ function gameInitit ()
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying  = true;
 
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
