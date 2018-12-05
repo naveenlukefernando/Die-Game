bb1c9a5e0db9a6ee12c6becddca27018678b2b65
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-12-05 14:04:23 +0530

    commit 2.04

--- app.js
+++ app.js
@@ -64,6 +64,8 @@ document.querySelector('.btn-hold').addEventListener('click',function() {
     {
         document.querySelector('#name-'+ activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+ activePlayer + '-panel').classList.add("winner");
+        document.querySelector('.player-'+ activePlayer + '-panel').classList.remove("active");
     }
     else{
         nextPlayer();
