d1306bb7ba12bd68147630e6b5aa654756da3079
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-12-05 14:31:24 +0530

    done 2.31

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ GAME RULES:
 
 var scores,roundScore, activePlayer ;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+gameInitit ();
+
 
 
 
@@ -97,18 +96,25 @@ function nextPlayer ()
 
 
 
-document.querySelector('.btn-new').addEventListener('click',function(){
+document.querySelector('.btn-new').addEventListener('click',gameInitit);
+
+
+function gameInitit ()
+{
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
 
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
-
-
-});
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove("winner");
+    document.querySelector('.player-1-panel').classList.remove("winner");
+    document.querySelector('.player-0-panel').classList.remove("active");
+    document.querySelector('.player-1-panel').classList.remove("active");
+    document.querySelector('.player-0-panel').classList.add("active");
+}
