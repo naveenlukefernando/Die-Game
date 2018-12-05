e7e920807006e12986bc4a6e92ecee510e3bbdf7
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-12-05 12:37:11 +0530

    changed 12.37

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
 document.querySelector('.btn-roll').addEventListener('click',function () {
 
     var dice = Math.floor(Math.random() * 6) + 1;
